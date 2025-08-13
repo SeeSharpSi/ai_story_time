8f3bf429b0a8aeb34b3c498d8c810d2bcd1f2a39
Silas Tompkins <silastompkins@outlook.com>
2025-08-13 16:32:13 -0400

    rewrite mostly done

--- templates/story_view_templ.go
+++ templates/story_view_templ.go
@@ -80,14 +80,14 @@ func StoryView(initialStory string, playerStatus story.PlayerStatus, inventory [
 			return templ_7745c5c3_Err
 		}
 		if len(playerStatus.Conditions) > 0 {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "<span class=\"condition\">| ")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "<span>| </span><span class=\"condition\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(strings.Join(playerStatus.Conditions, ", "))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/story_view.templ`, Line: 22, Col: 75}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/story_view.templ`, Line: 22, Col: 88}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
